482c892bc2929415dd85b5a35dcc858b39c0a27c
vivekvvarshney <4146952+vivekvvarshney@users.noreply.github.com>
2019-11-13 23:18:12 +0530

    Difficulty Changer added

--- Assets/Scripts/DifficultyScript.cs
+++ Assets/Scripts/DifficultyScript.cs
@@ -10,8 +10,10 @@ public class DifficultyScript : MonoBehaviour
     public Button ResetLevel;
     public Button ExitButton;
     public GameObject MazePrefab;
+    public GameObject BallPrefab;
     public Text DebugData;
-    public int difficultyLevel=0;
+    public int baseDifficultyLevel;
+    public int newDifficultyLevel;
     public float gameStartTime;
     public float gameEndTime;
     public float levelStartTime;
@@ -29,6 +31,9 @@ public class DifficultyScript : MonoBehaviour
         levelCounter = 0;
         tryCounter = 1;
         interactionTime = 0;
+        baseDifficultyLevel = 0;
+        newDifficultyLevel = 0;
+        ResetLevel.onClick.AddListener(ResetLevelfunction);
     }
 
     // Update is called once per frame
@@ -37,30 +42,38 @@ public class DifficultyScript : MonoBehaviour
         levelEndTime = Time.time;
         gameEndTime = Time.time;
         coinCounter = GameObject.FindGameObjectsWithTag("Coin").Length;
-        DebugData.text = "Difficulty Level: " + difficultyLevel + "\nLevelTime: " + (levelEndTime - levelStartTime) + "seconds\nGameTime: " +(gameEndTime-gameStartTime)+"seconds\nLevel Counter: "+levelCounter+"\nTry Counter: "+tryCounter+"\nCoins Left: "+coinCounter+ "\nInteractionTime: "+interactionTime + "\nLNumber of Mazes: " + GameObject.FindGameObjectsWithTag("Maze").Length;
+        DebugData.text = "Base Difficulty Level: " + baseDifficultyLevel + "\nNew Difficulty Level: " + newDifficultyLevel + "\nLevelTime: " + (levelEndTime - levelStartTime) + "seconds\nGameTime: " +(gameEndTime-gameStartTime)+"seconds\nLevel Counter: "+levelCounter+"\nTry Counter: "+tryCounter+"\nCoins Left: "+coinCounter+ "\nInteractionTime: "+interactionTime + "seconds\nRecognitionTime: " + (levelEndTime - levelStartTime- interactionTime) + "seconds\nNumber of Mazes: " + GameObject.FindGameObjectsWithTag("Maze").Length;
         
         ResetDifficulty.onClick.AddListener(ResetMaze);
-        ResetLevel.onClick.AddListener(ResetLevelfunction);
+       
         ExitButton.onClick.AddListener(ExitLevel);
         
         if(Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.RightArrow))
         {
             interactionTime = interactionTime + 0.01f;
         }
+        islevelCompleted();
     }
     
     void ResetMaze()
     {
-        SceneManager.LoadScene(0);
+        //SceneManager.LoadScene(0);
+        baseDifficultyLevel = 0;
+        newDifficultyLevel = 0;
+        gameStartTime = Time.time;
+        levelStartTime = Time.time;
+        levelCounter = 0;        
+        interactionTime = 0;
+        ResetLevelfunction();
+        tryCounter = 1;
 
     }
     void ResetLevelfunction()
     {
         ++tryCounter;
         Destroy(GameObject.FindGameObjectWithTag("Maze"));
-        //Destroy(GameObject.FindGameObjectWithTag("Maze"));
-        //Debug.Log("after destroy,Number of mazes: " + GameObject.FindGameObjectsWithTag("Maze").Length);
-        if (GameObject.FindGameObjectsWithTag("Maze").Length<=1)
+        Destroy(GameObject.FindGameObjectWithTag("Ball"));
+        if (GameObject.FindGameObjectsWithTag("Maze").Length>1)
         {
             Destroy(GameObject.FindGameObjectWithTag("Maze"));
             
@@ -68,6 +81,7 @@ public class DifficultyScript : MonoBehaviour
         else
         {
             Instantiate(MazePrefab);
+            Instantiate(BallPrefab);
             Debug.Log("after instance,Number of mazes: " + GameObject.FindGameObjectsWithTag("Maze").Length);
             
         }
@@ -79,4 +93,33 @@ public class DifficultyScript : MonoBehaviour
         Application.Quit(0);
 
     }
+
+    void measureChangeInDifficulty()
+    {
+        newDifficultyLevel++;
+        if (tryCounter > 2)
+        {
+            newDifficultyLevel--;
+            if (newDifficultyLevel == 0)
+                newDifficultyLevel = 0;
+        }
+        if(levelEndTime-levelStartTime-interactionTime<10)
+        {
+            newDifficultyLevel++;
+        }
+
+    }
+
+    void islevelCompleted()
+    {
+        if(GameObject.FindGameObjectsWithTag("Coin").Length==0)
+        {
+            ResetLevelfunction();
+            tryCounter = 1;
+            measureChangeInDifficulty();
+
+        }
+
+    }
+    
 }
---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    public Button ResetLevel;
+    public GameObject GObject;
+    int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ResetLevel.onClick.AddListener(ResetLevelMethod);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+    public void ResetLevelMethod()
+    {
+        //GObject.SetActive(false);
+        while(GameObject.FindGameObjectsWithTag("Maze").Length<1)
+        Destroy(GameObject.FindGameObjectWithTag("Maze"));
+
+        Debug.Log("Reset Level"+(++counter));
+        Instantiate(GObject);
+    }
+}
